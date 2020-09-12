86b4035ce394e9bb0a803c86750b914ef04e8242
Abhik1998 <abhik1998chakraborty@gmail.com>
2020-09-12 19:09:06 +0530

    more of practise

---
+++ cdf/670/a1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class a1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            
+            
+        }
+        sc.close();
+    }
+}
---
+++ cdf/670/a5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class a5{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            
+        }
+        sc.close();
+    }
+}
